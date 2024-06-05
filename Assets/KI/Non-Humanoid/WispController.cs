58b15665acd201468e4e499aef4ef1c9f47dd82d
RDMJoker <45975936+RDMJoker@users.noreply.github.com>
2024-06-05 12:37:47 +0200

    Wisp Movment & Anim

---
+++ Assets/KI/Non-Humanoid/WispController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+using Random = UnityEngine.Random;
+
+namespace KI.Non_Humanoid
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class WispController : MonoBehaviour
+    {
+        [SerializeField] float flyRange;
+        [SerializeField] float flySpeed;
+        [SerializeField] float flyPointDistanceThreshhold;
+        Vector3 spawnPosition;
+        NavMeshAgent agent;
+
+        void Awake()
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agent.speed = flySpeed;
+            spawnPosition = transform.position;
+            GetRandomPoint();
+        }
+
+        void FixedUpdate()
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance || agent.hasPath == false) GetRandomPoint();
+        }
+
+        void GetRandomPoint()
+        {
+            Vector3 randomPoint;
+            do
+            {
+                var unitSphere = Random.insideUnitSphere * flyRange;
+                randomPoint = new Vector3(unitSphere.x, 0, unitSphere.z);
+                randomPoint += spawnPosition;
+            } while (!NavMesh.SamplePosition(randomPoint, out _, agent.radius * 2, agent.areaMask) || Vector3.Distance(transform.position, randomPoint) < flyPointDistanceThreshhold);
+
+            agent.SetDestination(randomPoint);
+        }
+        void OnValidate()
+        {
+            flyPointDistanceThreshhold = Mathf.Clamp(flyPointDistanceThreshhold, 1, flyRange - 1);
+        }
+        
+        void OnDrawGizmos()
+        {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawWireSphere(transform.position, flyRange);
+            // Gizmos.color = Color.yellow;
+            // Gizmos.DrawSphere(transform.position + Vector3.up, AttackRange);
+        }
+        
+    }
+}
---
+++ Assets/KI/Non-Humanoid/WispHoverMovement.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace KI.Non_Humanoid
+{
+    public class WispHoverMovement : MonoBehaviour
+    {
+        [SerializeField] float yMovementFrequence;
+        [SerializeField] float yMovementAmplitude;
+
+        float startY;
+
+        void Awake()
+        {
+            startY = transform.position.y;
+        }
+
+
+        void FixedUpdate()
+        {
+            transform.position = new Vector3(transform.position.x, startY + Mathf.Sin(Time.time * yMovementFrequence) * yMovementAmplitude, transform.position.z);
+        }
+    }
+}
