03a5685204ce37514cbccee4eb5c247c0098ff06
RDMJoker <45975936+RDMJoker@users.noreply.github.com>
2024-06-20 15:59:49 +0200

    Level & Dungeon Gen remake

--- Assets/Scripts/Generation/DungeonGenerator/DungeonGenerator.cs
+++ Assets/Scripts/Generation/DungeonGenerator/DungeonGenerator.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using DefaultNamespace.Enums;
-using LL_Unity_Utils.Generic;
+﻿using Cinemachine;
 using NaughtyAttributes;
 using UnityEngine;
 
@@ -10,148 +6,18 @@ namespace Generation.DungeonGenerator
 {
     public class DungeonGenerator : MonoBehaviour
     {
-        [SerializeField] int generationWidth;
-        [SerializeField] int generationHeight;
-        [SerializeField] int minRoomCount;
-        [SerializeField] int level;
-        [SerializeField] float roomCountIncrementPerLevel;
-        [SerializeField] int seed;
-
-        ObjectGrid<ERoomTypes> grid;
-        System.Random random;
-        List<Vector2Int> endRooms;
-
+        [SerializeField] int levelAmount;
+        
+        
         [Button]
-        void ResetSeed()
+        void GenerateDungeon()
         {
-            seed = 0;
-        }
-
-        [Button]
-        void InitMap()
-        {
-            if (seed == 0) seed = System.DateTime.Now.Millisecond;
-            random = new System.Random(seed);
-            endRooms = new List<Vector2Int>();
-            grid = new ObjectGrid<ERoomTypes>(generationWidth, generationHeight);
-            ResetMap();
-            GenerateLevelMap(level);
-            Debug.Log(GetLevelLog());
-        }
-
-        void ResetMap()
-        {
-            endRooms.Clear();
-            for (int y = 0; y < generationHeight; y++)
+            var levelGenerator = GetComponent<LevelGenerator>();
+            for (int i = 1; i < levelAmount + 1; i++)
             {
-                for (int x = 0; x < generationWidth; x++)
-                {
-                    grid.SetValue(x, y, ERoomTypes.Free);
-                }
+                levelGenerator.ResetSeed();
+                levelGenerator.InitMap(i);
             }
-        }
-
-        void GenerateLevelMap(int _level)
-        {
-            int roomCountToGenerate = (int)(random.Next(0, 2) + minRoomCount + level * roomCountIncrementPerLevel);
-            var center = new Vector2Int(generationWidth, generationHeight) / 2;
-            bool isValid = false;
-            const int maxIterations = 100;
-            int currentIterations = 0;
-            while (!isValid && currentIterations < maxIterations)
-            {
-                ResetMap();
-                int rooms = GenerateRooms(roomCountToGenerate, center);
-                isValid = ValidateMap(roomCountToGenerate, rooms);
-                ++currentIterations;
-            }
-
-            if (currentIterations >= maxIterations) Debug.LogError("Something went wrong!!" + seed);
-            Debug.Log("Generated after " + currentIterations + " iterations!");
-            GenerateSpecialRooms();
-        }
-
-        bool ValidateMap(int _roomCountToGenerate, int _generatedRooms)
-        {
-            return _generatedRooms == _roomCountToGenerate && endRooms.Count >= 2;
-        }
-
-        void GenerateSpecialRooms()
-        {
-            var lastRoom = endRooms.Last();
-            grid.SetValue(lastRoom, ERoomTypes.Boss);
-        }
-
-        int GenerateRooms(int _roomsToGenerate, Vector2Int _center)
-        {
-            grid.SetValue(_center, ERoomTypes.Normal);
-
-            Queue<Vector2Int> discoveryQueue = new();
-            discoveryQueue.Enqueue(_center);
-
-            int generatedRooms = 1;
-            while (discoveryQueue.Count > 0)
-            {
-                var currentCoord = discoveryQueue.Dequeue();
-                var neighbourCoords = new[]
-                {
-                    currentCoord + Vector2Int.right,
-                    currentCoord + Vector2Int.left,
-                    currentCoord + Vector2Int.up,
-                    currentCoord + Vector2Int.down,
-                };
-                bool hasGenerated = false;
-
-                foreach (var currentNeighbourCoords in neighbourCoords)
-                {
-                    if (grid.IsOutsideBounds(currentNeighbourCoords)) continue;
-                    if (generatedRooms >= _roomsToGenerate) break;
-                    if (grid.GetValue(currentNeighbourCoords) != ERoomTypes.Free) continue;
-                    if (random.Next(0, 2) == 0) continue;
-                    if (GetNeighbourCount(currentNeighbourCoords) > 1) continue;
-
-                    grid.SetValue(currentNeighbourCoords, ERoomTypes.Normal);
-                    ++generatedRooms;
-                    discoveryQueue.Enqueue(currentNeighbourCoords);
-                    hasGenerated = true;
-                }
-
-                if (hasGenerated == false) endRooms.Add(currentCoord);
-            }
-
-            return generatedRooms;
-        }
-
-        int GetNeighbourCount(Vector2Int _coord)
-        {
-            var neighbourCoords = new Vector2Int[]
-            {
-                _coord + Vector2Int.right,
-                _coord + Vector2Int.left,
-                _coord + Vector2Int.up,
-                _coord + Vector2Int.down,
-            };
-
-            return neighbourCoords.Count(currentCoord => grid.GetValue(currentCoord) != ERoomTypes.Free);
-        }
-
-        string GetLevelLog()
-        {
-            var stringBuilder = new StringBuilder();
-            for (int y = 0; y < generationHeight; y++)
-            {
-                stringBuilder.Append("|");
-                for (int x = 0; x < generationWidth; x++)
-                {
-                    int value = (int)grid.GetValue(x, y);
-                    if (value >= 0) stringBuilder.Append(" ");
-                    stringBuilder.Append(value.ToString() + "|");
-                }
-
-                stringBuilder.Append("\n");
-            }
-
-            return stringBuilder.ToString();
         }
     }
 }
---
+++ Assets/Scripts/Generation/DungeonGenerator/LevelGenerator.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using DefaultNamespace.Enums;
+using LL_Unity_Utils.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Generation.DungeonGenerator
+{
+    public class LevelGenerator : MonoBehaviour
+    {
+        [SerializeField] int generationWidth;
+        [SerializeField] int generationHeight;
+        [SerializeField] int minRoomCount;
+        [SerializeField] float roomCountIncrementPerLevel;
+        [SerializeField] int seed;
+
+        ObjectGrid<ERoomTypes> grid;
+        System.Random random;
+        List<Vector2Int> endRooms;
+
+        [Button]
+        public void ResetSeed()
+        {
+            seed = 0;
+        }
+
+        [Button]
+        public void InitMap(int _level = 1)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            if (seed == 0) seed = System.DateTime.Now.Millisecond;
+            random = new System.Random(seed);
+            endRooms = new List<Vector2Int>();
+            grid = new ObjectGrid<ERoomTypes>(generationWidth, generationHeight);
+            ResetMap();
+            GenerateLevelMap(_level);
+            Debug.Log(GetLevelLog());
+            stopwatch.Stop();
+            Debug.Log(stopwatch.ElapsedMilliseconds);
+        }
+
+        void ResetMap()
+        {
+            endRooms.Clear();
+            for (int y = 0; y < generationHeight; y++)
+            {
+                for (int x = 0; x < generationWidth; x++)
+                {
+                    grid.SetValue(x, y, ERoomTypes.Free);
+                }
+            }
+        }
+
+        void GenerateLevelMap(int _level)
+        {
+            int roomCountToGenerate = (int)(random.Next(0, 2) + minRoomCount + _level * roomCountIncrementPerLevel);
+            var center = new Vector2Int(generationWidth, generationHeight) / 2;
+            bool isValid = false;
+            const int maxIterations = 100;
+            int currentIterations = 0;
+            while (!isValid && currentIterations < maxIterations)
+            {
+                ResetMap();
+                int rooms = GenerateRooms(roomCountToGenerate, center);
+                isValid = ValidateMap(roomCountToGenerate, rooms);
+                ++currentIterations;
+            }
+
+            if (currentIterations >= maxIterations) Debug.LogError("Something went wrong!!" + seed);
+            Debug.Log("Generated after " + currentIterations + " iterations!");
+            GenerateSpecialRooms();
+        }
+
+        bool ValidateMap(int _roomCountToGenerate, int _generatedRooms)
+        {
+            return _generatedRooms == _roomCountToGenerate && endRooms.Count >= 2;
+        }
+
+        void GenerateSpecialRooms()
+        {
+            var lastRoom = endRooms.Last();
+            grid.SetValue(lastRoom, ERoomTypes.Boss);
+        }
+
+        int GenerateRooms(int _roomsToGenerate, Vector2Int _center)
+        {
+            grid.SetValue(_center, ERoomTypes.Normal);
+
+            Queue<Vector2Int> discoveryQueue = new();
+            discoveryQueue.Enqueue(_center);
+
+            int generatedRooms = 1;
+            while (discoveryQueue.Count > 0)
+            {
+                var currentCoord = discoveryQueue.Dequeue();
+                var neighbourCoords = new[]
+                {
+                    currentCoord + Vector2Int.right,
+                    currentCoord + Vector2Int.left,
+                    currentCoord + Vector2Int.up,
+                    currentCoord + Vector2Int.down,
+                };
+                bool hasGenerated = false;
+
+                foreach (var currentNeighbourCoords in neighbourCoords)
+                {
+                    if (grid.IsOutsideBounds(currentNeighbourCoords)) continue;
+                    if (generatedRooms >= _roomsToGenerate) break;
+                    if (grid.GetValue(currentNeighbourCoords) != ERoomTypes.Free) continue;
+                    if (random.Next(0, 2) == 0) continue;
+                    if (GetNeighbourCount(currentNeighbourCoords) > 1) continue;
+
+                    grid.SetValue(currentNeighbourCoords, ERoomTypes.Normal);
+                    ++generatedRooms;
+                    discoveryQueue.Enqueue(currentNeighbourCoords);
+                    hasGenerated = true;
+                }
+
+                if (hasGenerated == false) endRooms.Add(currentCoord);
+            }
+
+            return generatedRooms;
+        }
+
+        int GetNeighbourCount(Vector2Int _coord)
+        {
+            var neighbourCoords = new Vector2Int[]
+            {
+                _coord + Vector2Int.right,
+                _coord + Vector2Int.left,
+                _coord + Vector2Int.up,
+                _coord + Vector2Int.down,
+            };
+
+            return neighbourCoords.Count(currentCoord => grid.GetValue(currentCoord) != ERoomTypes.Free);
+        }
+
+        string GetLevelLog()
+        {
+            var stringBuilder = new StringBuilder();
+            for (int y = 0; y < generationHeight; y++)
+            {
+                stringBuilder.Append("|");
+                for (int x = 0; x < generationWidth; x++)
+                {
+                    int value = (int)grid.GetValue(x, y);
+                    if (value >= 0) stringBuilder.Append(" ");
+                    stringBuilder.Append(value.ToString() + "|");
+                }
+
+                stringBuilder.Append("\n");
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
